2316343eac963f3d546110fe81face4be60001f2
thamyris <thamyriscabral@hotmail.com>
2023-03-15 14:27:16 -0300

    feat: criado componente List

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter';
 import Heading from './components/Heading';
+import List from './components/List';
 import Section from './components/Section';
 
 function App() {
@@ -12,6 +13,11 @@ function App() {
       <Heading title={'Hello'} />
       <Section title='Title Section'>This is my Section</Section>
       <Counter setCount={setCount}>Count {count}</Counter>
+      <List
+        item={['item1', 'item2', 'item3']}
+        render={(item: string) => <span>{item}</span>}
+        batata='batata'
+      />
     </div>
   );
 }
---
+++ src/components/List.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from 'react';
+
+interface ListTypes<T> {
+  item: T[];
+  render: (item: T) => ReactNode;
+  batata: string;
+}
+
+const List = <T,>({ item, render, batata }: ListTypes<T>) => {
+  return (
+    <div>
+      <ul>
+        {item.map((item, i) => (
+          <li key={i}> {render(item)}</li>
+        ))}
+      </ul>
+      {batata}
+    </div>
+  );
+};
+
+export default List;
